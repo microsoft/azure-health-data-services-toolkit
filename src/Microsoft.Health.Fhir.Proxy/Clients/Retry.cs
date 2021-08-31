523d339248c89a7688918b41277c240d6c3c7d1c
Matt Long <malong@microsoft.com>
2021-08-31 15:04:26 -0500

    retry

---
+++ src/Microsoft.Health.Fhir.Proxy/Clients/Retry.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Health.Fhir.Proxy.Clients
+{
+    public static class Retry
+    {
+        public static void Execute(Action retryOperation)
+        {
+            Execute(retryOperation, TimeSpan.FromMilliseconds(250), 3);
+        }
+
+        public static void Execute(Action retryOperation, TimeSpan deltaBackoff, int maxRetries)
+        {
+            int delayMilliseconds = Convert.ToInt32(deltaBackoff.TotalMilliseconds);
+            if (maxRetries < 1)
+            {
+                throw new ArgumentOutOfRangeException("Retry maxRetries must be >= 1.");
+            }
+
+            int attempt = 0;
+
+            while (attempt < maxRetries)
+            {
+                try
+                {
+                    retryOperation();
+                    return;
+                }
+                catch
+                {
+                    if (attempt == maxRetries)
+                    {
+                        throw;
+                    }
+
+                    Task.Delay(delayMilliseconds).GetAwaiter();
+                    attempt++;
+                }
+            }
+
+            throw new OperationCanceledException("Operation cancelled due to retry failure.");
+        }
+
+        public static async Task ExecuteAsync(Action retryOperation)
+        {
+            await ExecuteAsync(retryOperation, TimeSpan.FromMilliseconds(250), 3);
+        }
+
+        public static async Task ExecuteAsync(Action retryOperation, TimeSpan deltaBackoff, int maxRetries, ILogger logger = null)
+        {
+            int delayMilliseconds = Convert.ToInt32(deltaBackoff.TotalMilliseconds);
+
+            if (maxRetries < 1)
+            {
+                throw new ArgumentOutOfRangeException("Retry maxRetries must be >= 1.");
+            }
+
+            int attempt = 0;
+
+            while (attempt < maxRetries)
+            {
+                try
+                {
+                    retryOperation();
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    if (attempt == maxRetries)
+                    {
+                        logger?.LogWarning($"Retry max attempts {maxRetries} exceeded.");
+                        logger?.LogError(ex, "Retry max attempts exceeded.");
+                        throw;
+                    }
+                    else
+                    {
+                        logger?.LogWarning($"Retry attempt {attempt + 1} with delay {delayMilliseconds}ms.");
+                        logger?.LogError(ex, "Retrying due to exception.");
+                    }
+
+                    await Task.Delay(delayMilliseconds);
+                    attempt++;
+                }
+            }
+        }
+    }
+}
