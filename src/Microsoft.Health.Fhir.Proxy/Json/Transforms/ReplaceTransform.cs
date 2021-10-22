2a18ab5b521a58e34b9921f037cfc3175b20d3c3
Matt Long <malong@microsoft.com>
2021-10-22 07:59:18 -0500

    transform

---
+++ src/Microsoft.Health.Fhir.Proxy/Json/Transforms/ReplaceTransform.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Microsoft.Health.Fhir.Proxy.Json.Transforms
+{
+    [Serializable]
+    [JsonObject]
+    public class ReplaceTransform : Transform
+    {
+        [JsonProperty("name")]
+        public override string Name => "replace";
+
+        [JsonProperty("replaceNode")]
+        public string ReplaceNode { get; set; }
+
+        public override JObject Execute(string json)
+        {
+            JToken replaceNode = JToken.Parse(ReplaceNode);
+            JObject jobj = JObject.Parse(json);
+            JToken? token = jobj.Exists(JsonPath) ? jobj.SelectToken(JsonPath) : null;
+
+            if (token.IsNullOrEmpty())
+            {
+                return jobj;
+            }
+
+            jobj.SelectToken(JsonPath).Replace(replaceNode);
+
+            return jobj;
+        }
+    }
+}
---
+++ src/Microsoft.Health.Fhir.Proxy/Json/Transforms/Transform.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Microsoft.Health.Fhir.Proxy.Json.Transforms
+{
+    [Serializable]
+    [JsonConverter(typeof(TransformConverter))]
+    public abstract class Transform
+    {
+        [JsonProperty("name")]
+        public abstract string Name { get; }
+
+        [JsonProperty("jsonPath")]
+        public virtual string JsonPath { get; set; }
+
+        public abstract JObject Execute(string json);
+    }
+}
