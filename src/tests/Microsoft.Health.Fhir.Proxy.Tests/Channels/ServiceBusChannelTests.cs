d155fc7861a7a6e2905defa49c14c28b7011dca3
Matt Long <malong@microsoft.com>
2022-01-08 07:13:33 -0600

    config

--- src/tests/Microsoft.Health.Fhir.Proxy.Tests/Channels/ServiceBusChannelTests.cs
+++ src/tests/Microsoft.Health.Fhir.Proxy.Tests/Channels/ServiceBusChannelTests.cs
@@ -1,12 +1,9 @@
 ﻿using Azure.Messaging.ServiceBus;
-using Castle.Core.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Options;
 using Microsoft.Health.Fhir.Proxy.Channels;
-using Microsoft.Health.Fhir.Proxy.Configuration;
 using Microsoft.Health.Fhir.Proxy.Extensions.Channels;
 using Microsoft.Health.Fhir.Proxy.Storage;
 using Microsoft.Health.Fhir.Proxy.Tests.Assets;
