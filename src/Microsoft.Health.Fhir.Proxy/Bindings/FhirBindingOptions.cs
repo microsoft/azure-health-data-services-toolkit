f50b920a8fd6b4f0b505df5e3d17b43a9e6a4422
Matt Long <malong@microsoft.com>
2022-01-14 05:47:06 -0600

    rm routeprefix

--- src/Microsoft.Health.Fhir.Proxy/Bindings/FhirBindingOptions.cs
+++ src/Microsoft.Health.Fhir.Proxy/Bindings/FhirBindingOptions.cs
@@ -6,20 +6,11 @@
     public class FhirBindingOptions
     {
 
-        private string routePrefix = "fhir";
         /// <summary>
         /// Gets or sets the URL of the FHIR server.
         /// </summary>
         public string FhirServerUrl { get; set; }
-
-        /// <summary>
-        /// Gets or sets the route prefix. The default is fhir.
-        /// </summary>
-        public string RoutePrefix
-        {
-            get { return routePrefix; }
-            set { routePrefix = value; }
-        }
+       
 
     }
 }
