ce4cbb6f085827ab8253c70831d1fb217db76aa5
Mikael Weaver <mikael.weaver@microsoft.com>
2022-08-10 11:41:08 -0700

    Added bicep templates

---
+++ samples/PatientEverything/src/Configuration/MyServiceConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace PatientEverything.Configuration
+{
+    public class MyServiceConfig
+    {
+        public string FhirServerUrl { get; set; }
+        public string InstrumentationKey { get; set; }
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+
+
+    }
+}
