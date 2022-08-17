63e51577ee64d794c28cb444e9c7926f52d23da0
Mikael Weaver <mikael.weaver@microsoft.com>
2022-08-17 08:38:09 -0700

    manual merge

---
+++ samples/Authenticator/MyServiceOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthenticatorSample
+{
+    // This class has constrained configuration that is only used by "MyService".
+
+    public class MyServiceOptions
+    {
+        public string FhirServerUrl { get; set; }
+    }
+}
