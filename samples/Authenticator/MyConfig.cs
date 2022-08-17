3bf4a3382af42123d13efc752f1259ed5f32abb1
Mikael Weaver <mikael.weaver@microsoft.com>
2022-08-17 08:34:39 -0700

    manual merge fixes

---
+++ samples/Authenticator/MyConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthenticatorSample
+{
+    // This class holds the configuration for the application that
+    // needs to be set at runtime.
+    public class MyConfig
+    {
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string FhirServerUrl { get; set; }
+    }
+}
