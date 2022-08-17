908c370f16e843d008bd69a4e7e1d7371b516931
Shashank Narang <v-snarang@microsoft.com>
2022-08-17 11:59:01 +0530

    Updated the Env Variable to 'AZURE'

--- samples/BlobChannel/Program.cs
+++ samples/BlobChannel/Program.cs
@@ -14,7 +14,7 @@ using System.Text;
 
 IConfigurationBuilder configBuilder = new ConfigurationBuilder()
     .AddUserSecrets(Assembly.GetExecutingAssembly(), true)
-    .AddEnvironmentVariables("PROXY_");
+    .AddEnvironmentVariables("AZURE");
 IConfigurationRoot root = configBuilder.Build();
 BlobChannelConfig config = new();
 root.Bind(config);
