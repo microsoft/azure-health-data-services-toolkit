b4d35bfb4ccac0fcbb151773812521808964adf1
Mikael Weaver <mikael@mikael.dev>
2022-11-26 09:32:22 -0800

    Removed transfer encoding header

--- src/Microsoft.AzureHealth.DataServices.Core/Clients/RestRequestBuilder.cs
+++ src/Microsoft.AzureHealth.DataServices.Core/Clients/RestRequestBuilder.cs
@@ -165,6 +165,7 @@ namespace Microsoft.AzureHealth.DataServices.Clients
             {
                 Headers.Remove("Content-Type");
                 Headers.Remove("Content-Length");
+                Headers.Remove("Transfer-Encoding");
                 Headers.Remove("Authorization");
                 Headers.Remove("Accept");
                 Headers.Remove("Host");
